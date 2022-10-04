59f660eef0cd715d86fb5471b403fa4ddd885752
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 01:56:31 +0000

    Adding numeric_owner as keyword arguement

--- 1-build-crawler/OpenWPM/test/test_crawl.py
+++ 1-build-crawler/OpenWPM/test/test_crawl.py
@@ -87,7 +87,7 @@ class TestCrawl(OpenWPMTest):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, browser_params["0"]["profile_archive_dir"])
